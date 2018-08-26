7ef70bf3c5eddecfea8c4104622bd0fa9dc28e9a
alexsoleleris@gmail.com <alexsoleleris@gmail.com>
2018-08-26 23:15:05 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,17 +1,7 @@
-var name = "Susan"
+var name = "Joe"
 var height = "74"
 
 
 // Don't worry about this
 module.exports = { name, height
-}
-
-if (name ==="susan") {
-  console.log("The name is correct")
-}
-
-if (name ==="susan") {
-  console.log("the name is correct")
-} else {
-  console.log("expected the name to be Susan")
 }
